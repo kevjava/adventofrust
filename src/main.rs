9bdb0434a9a6c473cba3ef317bab352d8dd69678
Kevin Wallace <kevjava@gmail.com>
2022-12-16 20:01:34 -0600

    Final product with both stars on day 1.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use core::str;
-use std::fs;
+use std::{fs, io::Error};
 
+/// Returns an expect-able file result given a filename.
+fn open_file(filename: &str) -> Result<String, Error> {
+    return fs::read_to_string(filename);
+}
+
+/// Takes in a string, splits it by newlines, returns a Vec with the lines.
+fn split_lines(file_contents: &str) -> Vec<&str> {
+    return file_contents
+        .split("\n")
+        .collect();
+}
+
+/// Returns groups of lines, separated by blank lines.
+/// Splits one big Vec into a Vec of Vecs, containing the groups.
 fn get_groups(lines: Vec<&str>) -> Vec<Vec<&str>> {
     let mut groups : Vec<Vec<&str>> = Vec::new();
     let mut this_group: Vec<&str> = Vec::new();
     lines.into_iter().for_each(|x: &str| {
         if x.trim().is_empty() {
-            // let l = this_group.len();
             groups.push(this_group.to_vec());
-            // println!("Found {l} in that group.");
             this_group = Vec::new();
-            // println!("\n New Group \n");
         } else {
-            // println!("Found {x}");
             this_group.push(x);
         }
     });
@@ -21,7 +31,6 @@ fn get_groups(lines: Vec<&str>) -> Vec<Vec<&str>> {
 
 /// Returns a sum from a vector of strings.
 /// All strings will be parsed as i32 and summed up.
-/// 
 fn get_sum(group: Vec<&str>) -> i32  {
     let mut sum = 0;
     for line in group {
@@ -29,28 +38,31 @@ fn get_sum(group: Vec<&str>) -> i32  {
             .trim()
             .parse()
             .expect("Couldn't parse a number from the line.");
-        // println!("Adding {calories}");
         sum += calories;
     }
-    // println!("Returning {sum}\n");
     return sum;
 }
 
+/// Day 1 of Advent of Code 2022.
 fn main() {
-    let binding = fs::read_to_string("input.txt")
-        .expect("Should have been able to read the file.");
-    let contents : Vec<&str> = binding
-        .split("\n")
-        .collect();
+    let input_file = open_file("input.txt")
+        .expect("Couldn't open input file");
+    let contents = split_lines(&input_file);
 
     let groups : Vec<Vec<&str>> = get_groups(contents);
 
-    let mut biggest_sum = 0;
+    let mut list_of_sums: Vec<i32> = Vec::new();
     for group in groups {
         let sum : i32 = get_sum(group);
-        if sum > biggest_sum {
-            biggest_sum = sum;
-        }
+        list_of_sums.push(sum);
     }
-    println!("Biggest sum is {biggest_sum}");
+    list_of_sums.sort_by( |a,b| { b.cmp(a) }); // Reverse sort.
+
+    // for sum in &list_of_sums { print!("{sum} ")}
+    let biggest_sum: i32 = list_of_sums[0];
+    let top_3_sum: i32 = list_of_sums[0..3].to_vec().iter().sum();
+
+    println!("Biggest sum: {biggest_sum}");
+    println!("Sum of top three: {top_3_sum}");
 }
+
