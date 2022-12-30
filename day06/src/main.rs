70ead806bd843fd45255a1f30489b992a20879cf
Kevin Wallace <kevjava@gmail.com>
2022-12-30 09:15:42 -0600

    Completed day 6.

---
+++ day06/src/main.rs
@@ -0,0 +1,56 @@
+use std::collections::HashSet;
+
+mod input_file;
+
+fn main() -> Result<(), String> {
+    let infile = input_file::InputFile::new("input.txt");
+    
+    let lines = match infile.read_lines() {
+        Ok(lines) => { lines }
+        Err(message) => { return Err(format!("Error opening input file: {message}")) }
+    };
+
+    part1(&lines);
+    part2(&lines);
+    Ok(())
+}
+
+fn part1(lines: &Vec<String>) {
+    for line in lines {
+        let mut marker = 4;
+        loop {
+            if marker > line.len() { 
+                println!("Line: {line}: reached the end.");
+                break; 
+            }
+            let chars = line[marker-4..marker]
+                .chars()
+                .collect::<HashSet<char>>();
+            if chars.len() == 4 {
+                println!("Line: {line}: Marker: {marker}");
+                break;
+            }
+            marker += 1;
+        }
+    }
+}
+
+fn part2(lines: &Vec<String>) {
+    for line in lines {
+        let mut marker = 14;
+        loop {
+            if marker > line.len() { 
+                println!("Line: {line}: reached the end.");
+                break; 
+            }
+            let chars = line[marker-14..marker]
+                .chars()
+                .collect::<HashSet<char>>();
+            if chars.len() == 14 {
+                println!("Line: {line}: Marker: {marker}");
+                break;
+            }
+            marker += 1;
+        }
+    }
+}
